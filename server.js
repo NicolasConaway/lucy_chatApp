dd31b2804f6781aa4cd0b4ba22fb3e96feace84d
Nicolas Conaway <n.conaway00@gmail.com>
2020-05-04 12:34:55 -0400

    Lucy's node Chat app

---
+++ server.js
@@ -0,0 +1,63 @@
+const http = require('http')
+
+const path = require('path')
+
+const process = require('process')
+
+const { static } = require('./routes/static')
+
+const { add } = require('./routes/add')
+
+const { update } = require('./routes/update')
+
+const host = 'localhost'
+const port = 8000
+
+
+const routes = {
+    '/': (req, res) => static({
+        filename: path.resolve(__dirname, 'public/index.html'),
+        contentType: 'text/html'
+    }, res),
+    '/css/normalize.css': (req, res) => static ({
+        filename: path.resolve(__dirname, 'public/css/normalize.css'),
+        contentType: 'text/css'
+    }, res),
+    '/css/style.css': (req, res) => static ({
+        filename: path.resolve(__dirname, 'public/css/style.css'),
+        contentType: 'text/css'
+    }, res),
+    '/image/helpbot.jpg': (req, res) => static ({
+        filename: path.resolve(__dirname, '/image/helpbot.jpg'),
+        contentType: 'image/jpg'
+    }, res),
+    '/css/style.css': (req, res) => static ({
+        filename: path.resolve(__dirname, 'public/css/style.css'),
+        contentType: 'text/css'
+    }, res),
+}
+
+
+const server = http.createServer((req, res) => {
+    const url = require('url')
+    if (routes[url.pathname]){
+        return routes[url.pathname](req, res)
+    }
+    res.writeHead(404, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify({
+        response: 'failed',
+        data: null,
+        message: 'resource was not found'
+    }))
+})
+
+
+server.listen(port, host, () => {
+    console.log(`The server has started on 'http://${host}:${port}'`)
+})
+
+
+
+
+
+
